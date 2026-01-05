ae5707e782b44833a3c04b6447b72292e94eb7d2
toolsetlink <songang@MacBook-Pro-14.local>
2026-01-05 10:17:47 +0800

    fix bug

--- test/ClientTests.cs
+++ test/ClientTests.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Reflection;
 using NUnit.Framework;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using ToolsetLink.UpgradeLinkApi;
 using ToolsetLink.UpgradeLinkApi.Models;
 
@@ -8,6 +11,32 @@ namespace UpgradeLinkApi.Tests
     [TestFixture]
     public class ClientTests
     {
+        // 自定义JSON契约解析器，用于处理[NameInMap]属性
+        private class NameInMapContractResolver : DefaultContractResolver
+        {
+            protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+            {
+                JsonProperty property = base.CreateProperty(member, memberSerialization);
+                
+                // 查找NameInMap属性
+                var nameInMapAttribute = member.GetCustomAttribute(typeof(Tea.NameInMapAttribute)) as Tea.NameInMapAttribute;
+                if (nameInMapAttribute != null)
+                {
+                    // 使用NameInMap属性的值作为JSON字段名
+                    property.PropertyName = nameInMapAttribute.Name;
+                }
+                
+                return property;
+            }
+        }
+        
+        // 自定义JSON序列化配置
+        private static readonly JsonSerializerSettings _jsonSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new NameInMapContractResolver(),
+            NullValueHandling = NullValueHandling.Ignore
+        };
+        
         private Client _client;
         private Config _config;
 
@@ -53,20 +82,21 @@ namespace UpgradeLinkApi.Tests
                 Console.WriteLine($"  DevModelKey: {request.DevModelKey}");
                 Console.WriteLine($"  DevKey: {request.DevKey}");
                 
-                // 序列化请求体并打印
-                string bodyStr = AlibabaCloud.TeaUtil.Common.ToJSONString(request);
+                // 序列化请求体并打印（使用与客户端相同的自定义契约解析器）
+                string bodyStr = Newtonsoft.Json.JsonConvert.SerializeObject(request, _jsonSettings);
                 Console.WriteLine($"序列化后的请求体: {bodyStr}");
+                Console.WriteLine("注意：上面的请求体与实际请求使用的序列化格式一致");
                 
                 var response = _client.WinUpgrade(request);
                 Console.WriteLine("请求成功!");
-                Console.WriteLine($"响应代码: {response.Code}");
-                Console.WriteLine($"响应消息: {response.Msg}");
-                Console.WriteLine($"TraceId: {response.TraceId}");
+                Console.WriteLine($"response.code: {response.Code}");
+                Console.WriteLine($"response.msg: {response.Msg}");
+                Console.WriteLine($"response.traceId: {response.TraceId}");
                 
-                // 打印响应数据
+                // 打印响应数据（使用与客户端相同的自定义契约解析器）
                 if (response.Data != null)
                 {
-                    Console.WriteLine($"Data: {Newtonsoft.Json.JsonConvert.SerializeObject(response.Data)}");
+                    Console.WriteLine($"Data: {Newtonsoft.Json.JsonConvert.SerializeObject(response.Data, _jsonSettings)}");
                 }
             }
             catch (Tea.TeaException ex)
--- test/obj/Debug/net8.0/upgradeLinkApiCSharpTest.AssemblyInfo.cs
+++ test/obj/Debug/net8.0/upgradeLinkApiCSharpTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("upgradeLinkApiCSharpTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d39e66211db1d919e102d0d8216e0169531f06d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaead0cd9153071d6a2479d385409fb89258dcdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("upgradeLinkApiCSharpTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("upgradeLinkApiCSharpTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
